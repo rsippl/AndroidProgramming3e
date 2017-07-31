b4ae2cbc1715ccafb4f24763d3a3b7d92a7f63f6
Ralf Sippl <ralf.sippl@gmail.com>
2017-07-31 19:01:29 +0200

    Ch.1 challenge: customizing the toast

--- apps/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.kt
+++ apps/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_quiz.*
 
@@ -12,11 +13,15 @@ class QuizActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz)
 
         true_button.setOnClickListener {
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
         }
 
         false_button.setOnClickListener {
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            val toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
         }
     }
 }
