7ac60f26ad3369479d6bd6fb9ae489f8e3c0bca1
Ralf Sippl <ralf.sippl@gmail.com>
2017-08-02 12:30:10 +0200

    Ch.3: logging the activity lifecycle

--- apps/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.kt
+++ apps/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.kt
@@ -2,10 +2,13 @@ package com.bignerdranch.android.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizActivity : AppCompatActivity() {
+
+    private val TAG = "QuizActivity"
 
     private val questionBank: Array<Question> = arrayOf(
             Question(R.string.question_australia, true),
@@ -20,6 +23,7 @@ class QuizActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle) called")
         setContentView(R.layout.activity_quiz)
 
 
@@ -44,6 +48,31 @@ class QuizActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun showNextQuestion() {
